87bde3040a61d5000dcfa1dd434f119a896a15df
DOGNIMÉ KONÉ <dognime.kone@polymtl.ca>
2017-03-06 14:56:41 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,1 +1,44 @@
+resource "google_compute_instance" "dokonb-etcd1" {
+  name         = "dokonb-etcd1"
+  machine_type = "f1-micro"
+  zone         = "us-east1-b"
 
+  disk {
+    image = "coreos-cloud/coreos-stable"
+  }
+
+  network_interface {
+    subnetwork = "${google_compute_subnetwork.east1-dokonb-backend.name}"
+    access_config {
+    }
+  }
+}
+resource "google_compute_instance" "dokonb-etcd2" {
+  name         = "dokonb-etcd2"
+  machine_type = "f1-micro"
+  zone         = "us-east1-b"
+
+  disk {
+    image = "coreos-cloud/coreos-stable"
+  }
+  network_interface {
+    subnetwork = "${google_compute_subnetwork.east1-dokonb-backend.name}"
+    access_config {
+    }
+  }
+}
+resource "google_compute_instance" "dokonb-etcd3" {
+  name         = "dokonb-etcd3"
+  machine_type = "f1-micro"
+  zone         = "us-east1-b"
+
+  disk {
+    image = "coreos-cloud/coreos-stable"
+  }
+  network_interface {
+    subnetwork = "${google_compute_subnetwork.east1-dokonb-backend.name}"
+    access_config {
+
+    }
+  }
+}
