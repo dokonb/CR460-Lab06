4ded9980b41a2f81334a20a7151c4e10c550062d
DOGNIMÉ KONÉ <dognime.kone@polymtl.ca>
2017-03-08 13:43:56 -0500

    Update public.tf

--- public.tf
+++ public.tf
@@ -1,20 +1,19 @@
-resource "google_compute_instance" "jump.dokonb.cr460lab.com" {
-  name         = "jump.dokonb.cr460lab.com"
+resource "google_compute_instance" "jump" {
+  name         = "jump"
   machine_type = "f1-micro"
   zone         = "us-east1-b"
 
   disk {
-    image = "debian-8-jessie-v20170227"
+    image = "debian-cloud/debian-8"
   }
-
   network_interface {
     subnetwork = "${google_compute_subnetwork.east1-dokonb-public.name}"
     access_config {
     }
   }
 }
-resource "google_compute_instance" "vault.dokonb.cr460lab.com" {
-  name         = "vault.dokonb.cr460lab.com"
+resource "google_compute_instance" "vault" {
+  name         = "vault"
   machine_type = "f1-micro"
   zone         = "us-east1-b"
 
